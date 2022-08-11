46a1810ec692f7d1495002fae9bf1783c615f002
Jordan Lewis <jordan@forefrontweb.com>
2022-08-11 09:14:06 -0400

    Added proper icons

--- src/components/global/Footer/ContactUs.jsx
+++ src/components/global/Footer/ContactUs.jsx
@@ -35,9 +35,9 @@ const ContactUs = () => {
         <div className='site-footer-col'>
             <div className="site-footer-section">
                 {footerContactHeading && <h4 className='h3 site-footer-heading'>{footerContactHeading}</h4>}
-                {footerAddress && <a className={styles.siteFooterContactLink} href={footerAddress?.url} title={footerAddress?.title} target={footerAddress?.target}><span dangerouslySetInnerHTML={{ __html: footerAddress?.title }}></span></a>}
-                {footerEmailAddress && <a className={styles.siteFooterContactLink} href={footerEmailAddress?.url} target={footerEmailAddress?.target} title={footerEmailAddress?.title}><span>{footerEmailAddress?.title}</span></a>}
-                {footerPhoneNumber && <a className={styles.siteFooterContactLink} href={footerPhoneNumber?.url} target={footerPhoneNumber?.target} title={footerPhoneNumber?.title}><span>{footerPhoneNumber?.title}</span></a>}
+                {footerAddress && <a className={styles.siteFooterContactAddress} href={footerAddress?.url} title={footerAddress?.title} target={footerAddress?.target}><span dangerouslySetInnerHTML={{ __html: footerAddress?.title }}></span></a>}
+                {footerEmailAddress && <a className={styles.siteFooterContactEmail} href={footerEmailAddress?.url} target={footerEmailAddress?.target} title={footerEmailAddress?.title}><span>{footerEmailAddress?.title}</span></a>}
+                {footerPhoneNumber && <a className={styles.siteFooterContactPhone} href={footerPhoneNumber?.url} target={footerPhoneNumber?.target} title={footerPhoneNumber?.title}><span>{footerPhoneNumber?.title}</span></a>}
             </div>
         </div>
     )
